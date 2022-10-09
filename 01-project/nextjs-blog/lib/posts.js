d19080c289b5aa4e80ec2c14d2e8c40bf98e4b52
glaucia86 <gllemos@microsoft.com>
2022-10-08 22:14:16 -0300

    creating a parsed data as a sample.

--- 01-project/nextjs-blog/lib/posts.js
+++ 01-project/nextjs-blog/lib/posts.js
@@ -4,3 +4,38 @@
  * data: 10/08/2022
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
+
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { EOF } from 'dns';
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+    const fileNames = fs.readdirSync(postsDirectory);
+    const allPostsData = fileNames.map((fileName) => {
+        const id = fileName.replace(/\.md$/, '');
+
+        const fullPath = path.join(postsDirectory, fileName);
+        const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+        const matterResult = matter(fileContents);
+
+        return {
+            id,
+            ...matterResult.data,
+        };
+    });
+
+    // ==> Sorting posts by date:
+    return allPostsData.sort(({ date: a }, { date: b }) => {
+        if (a < b) {
+            return 1;
+        } else if (a > b) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+};
