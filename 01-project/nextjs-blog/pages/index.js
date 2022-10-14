c64b3457ca0a74c14d49acf838332beb0bfd367f
glaucia86 <glaucia_lemos86@hotmail.com>
2022-10-14 17:53:27 -0300

    polishing the 'index.js' file

--- 01-project/nextjs-blog/pages/index.js
+++ 01-project/nextjs-blog/pages/index.js
@@ -5,10 +5,12 @@
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/Layout/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
+import Link from "next/link";
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/Layout/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/Date/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -29,28 +31,30 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>
           Hello, I'm Glaucia Lemos. I'm a Developer Advocate at Microsoft. You
-          can contact me on{' '}
-          <a href='https://twitter.com/glaucia_lemos86'>Twitter</a>.
+          can contact me on{" "}
+          <a href="https://twitter.com/glaucia_lemos86">Twitter</a>.
         </p>
         <p>
-          And also, you can find me on{' '}
-          <a href='https://github.com/glaucia86'>GitHub</a>.
+          And also, you can find me on{" "}
+          <a href="https://github.com/glaucia86">GitHub</a>.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h2 className={utilStyles.headingLg}>Blog</h2>
-          <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, date, title }) => (
-              <li className={utilStyles.listItem} key={id}>
-                {title}
-                <br />
-                {id}
-                <br />
-                {date}
-              </li>
-            ))}
-          </ul>
-        </section>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
