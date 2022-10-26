36a4fc03ae2fbfbb8b67a89011b796cf9d1abe12
glaucia86 <glaucia_lemos86@hotmail.com>
2022-10-25 21:14:22 -0300

    configuring the application.

--- 03-project/material-ui-app/pages/_app.js
+++ 03-project/material-ui-app/pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseLine from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../config/theme';
+import createEmotionCache from '../config/createEmotionCache';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name='viewport' content='initial-scale=1 width=device-width' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <Component {...props} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
-
-export default MyApp
